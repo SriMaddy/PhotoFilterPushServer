80fe0b2ea130ce590f78a51cd2fc5d1548af09a4
karthick <sri.bharath72@gmail.com>
2018-04-10 18:05:16 +0530

    Token Registration Complete

--- src/main/java/com/example/dbaccess/token/NotificationController.java
+++ src/main/java/com/example/dbaccess/token/NotificationController.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.util.Calendar;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +21,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
 @RestController
@@ -28,44 +30,46 @@ public class NotificationController {
 
 	private static final String ANDROID_SERVER_KEY = "AIzaSyDmRAY_cW-qjNXmA-r28k0pTLdeXZnovpc";
 	private static final String URL = "https://fcm.googleapis.com/fcm/send";
-	
+
 	@RequestMapping(value = "/registerToken", method = org.springframework.web.bind.annotation.RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<TokenBean> registerToken(@RequestBody String payload) {		
+	public ResponseEntity<TokenBean> registerToken(@RequestBody String payload) {
 		Gson gson = new Gson();
 		TokenBean token = gson.fromJson(payload, TokenBean.class);
-		
+
 		final FirebaseDatabase database = FirebaseDatabase.getInstance();
 		DatabaseReference ref = database.getReference("monitor").child("token");
-		final ChildEventListener listener = new ChildEventListener() {
-			
+		ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
 			@Override
-			public void onChildRemoved(DataSnapshot arg0) {
+			public void onDataChange(DataSnapshot arg0) {
 				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onChildMoved(DataSnapshot arg0, String arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onChildChanged(DataSnapshot arg0, String arg1) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onChildAdded(DataSnapshot arg0, String arg1) {
-				// TODO Auto-generated method stub
-				TokenBean tokenSnapShot = arg0.getValue(TokenBean.class);
-				if(token.getDeviceId().equals(tokenSnapShot.getDeviceId())) {
-					if(!token.getToken().equals(tokenSnapShot.getToken())) {
-						DatabaseReference ref1 = database.getReference("monitor").child("token").push();
-						String key = ref1.getKey();
-						token.setId(key);
-						ref1.setValueAsync(token);
+				if (arg0.getChildrenCount() > 0) {
+					for (DataSnapshot snapshot : arg0.getChildren()) {
+						TokenBean tokenSnapShot = snapshot.getValue(TokenBean.class);
+
+						if (token.getDeviceId().equals(tokenSnapShot.getDeviceId())
+								&& token.getToken().equals(tokenSnapShot.getToken())) {
+							break;
+						} else {
+							for (DataSnapshot snapshot1 : arg0.getChildren()) {
+								TokenBean tokenSnapShot1 = snapshot1.getValue(TokenBean.class);
+
+								if (token.getDeviceId().equals(tokenSnapShot1.getDeviceId())) {
+									if (!token.getToken().equals(tokenSnapShot1.getToken())) {
+										DatabaseReference ref = database.getReference("monitor").child("token")
+												.child(snapshot.getKey());
+										ref.child("token").setValueAsync(token.getToken());
+										break;
+									}
+								} else {
+									DatabaseReference ref1 = database.getReference("monitor").child("token").push();
+									String key = ref1.getKey();
+									token.setId(key);
+									ref1.setValueAsync(token);
+									break;
+								}
+							}
+						}
 					}
 				} else {
 					DatabaseReference ref1 = database.getReference("monitor").child("token").push();
@@ -74,19 +78,17 @@ public class NotificationController {
 					ref1.setValueAsync(token);
 				}
 			}
-			
+
 			@Override
 			public void onCancelled(DatabaseError arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-		};
-		
-		ref.addChildEventListener(listener);
-		ref.removeEventListener(listener);
+		});
+
 		return null;
 	}
-	
+
 	@RequestMapping(value = "/sendNotification", method = org.springframework.web.bind.annotation.RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<NotificationBean> sendPushNotificationToFCMServer(@RequestBody String payload) {
 		Gson gson = new Gson();
@@ -94,8 +96,8 @@ public class NotificationController {
 		NotificationBean notification = notificationDataBean.getNotificationBean();
 		notification.setTimestamp(Calendar.getInstance().getTimeInMillis());
 		notificationDataBean.setNotificationBean(notification);
-		
-//		String payloadData = notification.toString();
+
+		// String payloadData = notification.toString();
 		String notificationDataStr = gson.toJson(notificationDataBean, NotificationDataBean.class);
 		String payloadData = notificationDataStr;
 		java.net.URL obj = null;
@@ -111,19 +113,19 @@ public class NotificationController {
 			con.setRequestMethod("POST");
 			con.setRequestProperty("Content-Type", "application/json; charset-utf8");
 			con.setRequestProperty("Authorization", "key=" + ANDROID_SERVER_KEY);
-			
+
 			// Send post request
 			con.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
 			wr.write(payloadData.getBytes("UTF-8"));
 			wr.flush();
 			wr.close();
-			
+
 			int responseCode = con.getResponseCode();
-//			logger.debug("Response Code : " + responseCode);
-//			logger.debug(payloadData);
+			// logger.debug("Response Code : " + responseCode);
+			// logger.debug(payloadData);
 			System.out.println("Response Code : " + responseCode);
-			
+
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
@@ -133,8 +135,8 @@ public class NotificationController {
 			}
 			in.close();
 			con.disconnect();
-			
-//			notification = notificationRepo.save(notification);
+
+			// notification = notificationRepo.save(notification);
 
 			System.out.println("Response : " + response.toString());
 		} catch (IOException e) {
